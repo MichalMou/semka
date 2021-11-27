ab5ba72b69fbd32530e54873fc7ba70a564296b6
MichalMou <ondas179@gmail.com>
2021-11-27 15:08:49 +0100

    info start

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilmyComponent } from './pages/filmy/filmy.component';
 import { HomeComponent } from './pages/home/home.component';
+import { InfoComponent } from './pages/info/info.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SerialsComponent } from './pages/serialy/serials.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
@@ -14,7 +15,8 @@ const routes: Routes = [
   //{ path: 'filmy/:nazov', component: FilmyComponent },
   { path: 'serialy', component: SerialsComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'sign-up', component: SignUpComponent }
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'info', component: InfoComponent }
   
 ];
 
